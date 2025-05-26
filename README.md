401d1f2995d623d13ad42d7c027118a5a2009b9b
ivan <vanamelcikov7275@gmail.com>
2025-05-26 21:32:47 +0400

    Fix structure

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # weather-telegram-bot
 
 Бот для получения прогноза погоды в Telegram.
+
+## Запуск
+
+```bash
+uv run -m weather_tg_bot
+```
---
+++ weather_tg_bot/__main__.py
@@ -0,0 +1,9 @@
+"""Запуск бота."""
+
+import asyncio
+
+from weather_tg_bot.main import main
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
