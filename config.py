0265fa93bfde4b0b4cc5b8247124a79aabd37565
ivan <vanamelcikov7275@gmail.com>
2025-05-26 21:21:10 +0400

    Add .env

--- config.py
+++ config.py
@@ -1,2 +1,21 @@
-open_weather_token = "openwether_token"
-tg_bot_token = "your_bot_token"
+"""Глобальные настройки бота."""
+
+from __future__ import annotations
+
+from pydantic_settings import BaseSettings
+
+
+class Config(BaseSettings):
+    """Глобальные настройки бота.
+
+    Подгружаются один раз при запуске бота из .env файла.
+
+    - tg_bot_token: Токен Telegram Bot API.
+    - open_weather_token: Токен open weather API.
+    """
+
+    tg_bot_token: str
+    open_weather_token: str
+
+
+config: Config = Config(_env_file=".env")
